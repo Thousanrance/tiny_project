a1efebb4c8640e58e40e30d71bd2508266d49863
Thousanrance <1473117314@qq.com>
2022-05-01 21:21:04 +0800

    2022/05/01

---
+++ build/tiny/include/tiny/ShapeInferenceOpInterfaces.cpp.inc.d
@@ -0,0 +1,1 @@
+tiny/include/tiny/ShapeInferenceOpInterfaces.cpp.inc: /home/thousanrance/llvm-project/mlir/include/mlir/IR/DialectBase.td /home/thousanrance/llvm-project/mlir/include/mlir/IR/OpBase.td
---
+++ build/tiny/include/tiny/ShapeInferenceOpInterfaces.h.inc.d
@@ -0,0 +1,1 @@
+tiny/include/tiny/ShapeInferenceOpInterfaces.h.inc: /home/thousanrance/llvm-project/mlir/include/mlir/IR/DialectBase.td /home/thousanrance/llvm-project/mlir/include/mlir/IR/OpBase.td
